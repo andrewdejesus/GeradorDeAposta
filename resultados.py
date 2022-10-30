65432d3986b13b68788c088e3802635c009228e4
Andrew Machado Dias de Jesus <andrewdias2016@gmail.com>
2022-10-29 21:31:50 -0300

    Update resultados.py

--- resultados.py
+++ resultados.py
@@ -1,3 +1,4 @@
+from logging import exception
 import requests
 from bs4 import *
 
@@ -7,23 +8,28 @@ def verificador(VERIF):
 
     JOGOS = []
 
-    while concurso != 2534:
-        
-        response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
-        dezenas = response.json()
-        if dezenas["dezenas"]:
-            JOGOS.append(dezenas["dezenas"])
-        else:
-            print(concurso)
+    while True:
+        try:
+            response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+            dezenas = response.json()
+            if dezenas["dezenas"]:
+                JOGOS.append(dezenas["dezenas"])
+            else:
+                print(concurso)
+                break
+            concurso += 1
+        except:
             break
-        concurso += 1
 
-
-    l1 = VERIF
-    for jogo in JOGOS:
-        if jogo == l1:
-            print("Jogo repetido: ")
-            print(jogo)
-            break
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
 
     print("FIM")
