38eee2ea27afcf29ae3fde614f6d41da07575058
Andrew Machado Dias de Jesus <andrewdias2016@gmail.com>
2022-11-20 16:14:22 -0300

    att

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
 from resultados import *
-
+import threading
 from gerarnumero import *
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 if __name__== "__main__":
-    while True:
-        jogo = input("Quantidade de números: ")
-        if jogo == "6":
-            verif = gerandonumero6(aposta)
-            verificador(verif)
-            print(verif)
-            break
-        if jogo == "7":
-            print(gerandonumero7(aposta))
-            break
-        if jogo == "8":
-            print(gerandonumero8(aposta))
-            break
-        if jogo == "9":
-            print(gerandonumero9(aposta))
-            break
-        if jogo == "10":
-            print(gerandonumero10(aposta))
-            break
-        if jogo == "11":
-            print(gerandonumero11(aposta))
-            break
-        if jogo == "12":
-            print(gerandonumero12(aposta))
-            break
-        if jogo == "13":
-            print(gerandonumero13(aposta))
-            break
-        if jogo == "14":
-            print(gerandonumero14(aposta))
-            break
-        if jogo == "15":
-            print(gerandonumero15(aposta))
-            break
-        else:
-            print("Digite um número de 6 a 15")
+    
+    jogo = input("Quantidade de números: ")
+    if jogo == "6":
+        VERIF = gerandonumero6(aposta)
+            
+        try:
+            t1 = threading.Thread(target=verificador1, args=[VERIF])
+            t2 = threading.Thread(target=verificador2, args=[VERIF])
+            t3 = threading.Thread(target=verificador3, args=[VERIF])
+            t4 = threading.Thread(target=verificador4, args=[VERIF])
+            t5 = threading.Thread(target=verificador5, args=[VERIF])
+            t6 = threading.Thread(target=verificador6, args=[VERIF])
+            t7 = threading.Thread(target=verificador7, args=[VERIF])
+            t8 = threading.Thread(target=verificador8, args=[VERIF])
+            t9 = threading.Thread(target=verificador9, args=[VERIF])
+            t10 = threading.Thread(target=verificador10, args=[VERIF])
+            t11 = threading.Thread(target=verificador11, args=[VERIF])
+            t1.start()
+            t2.start()
+            t3.start()
+            t4.start()
+            t5.start()
+            t6.start()
+            t7.start()
+            t8.start()
+            t9.start()
+            t10.start()
+            t11.start()
+            print("Comparando jogos...")
+        except Exception as e:
+            print(e)
+            
+            
+    if jogo == "7":
+        print(gerandonumero7(aposta))
+            
+    if jogo == "8":
+        print(gerandonumero8(aposta))
+            
+    if jogo == "9":
+        print(gerandonumero9(aposta))
+            
+    if jogo == "10":
+        print(gerandonumero10(aposta))
+            
+    if jogo == "11":
+        print(gerandonumero11(aposta))
+            
+    if jogo == "12":
+        print(gerandonumero12(aposta))
+            
+    if jogo == "13":
+        print(gerandonumero13(aposta))
+            
+    if jogo == "14":
+        print(gerandonumero14(aposta))
+            
+    if jogo == "15":
+        print(gerandonumero15(aposta))
+            
--- resultados.py
+++ resultados.py
@@ -3,13 +3,339 @@ import requests
 from bs4 import *
 
 
-def verificador(VERIF):
+def verificador1(VERIF):
     concurso = 1
 
     JOGOS = []
 
     while True:
         try:
+            if concurso == 200:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+    
+
+def verificador2(VERIF):
+    concurso = 200
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 400:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador3(VERIF):
+    concurso = 400
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 600:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador4(VERIF):
+    concurso = 600
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 800:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador5(VERIF):
+    concurso = 800
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1000:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador6(VERIF):
+    concurso = 1000
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1200:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador7(VERIF):
+    concurso = 1200
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1400:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador8(VERIF):
+    concurso = 1400
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1550:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador9(VERIF):
+    concurso = 1550
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1700:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+
+def verificador10(VERIF):
+    concurso = 1700
+
+    JOGOS = []
+
+    while True:
+        try:
+            if concurso == 1850:
+                break
+            else:
+                response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
+                dezenas = response.json()
+                if dezenas["dezenas"]:
+                    JOGOS.append(dezenas["dezenas"])
+                else:
+                    print(concurso)
+                    break
+                concurso += 1
+        except:
+            break
+
+    try:
+        l1 = VERIF
+        for jogo in JOGOS:
+            if jogo == l1:
+                print("Jogo repetido: ")
+                print(jogo)
+                break
+    except:
+        print("Algo deu errado")
+        pass
+    
+
+def verificador11(VERIF):
+    concurso = 1850
+
+    JOGOS = []
+
+    while True:
+        try:
+            
+            
+            
             response = requests.get(f"https://loteriascaixa-api.herokuapp.com/api/mega-sena/{concurso}", verify=False)
             dezenas = response.json()
             if dezenas["dezenas"]:
@@ -33,3 +359,5 @@ def verificador(VERIF):
         pass
 
     print("FIM")
+    print(VERIF)
+    
